9a459b2cfed7f8fd24cd9bb329daaad70e688a71
Kaiane Souza <kaiane.cordeiro@sou.inteli.edu.br>
2026-02-11 12:51:01 +0000

    feat: adds calendar, plants and settings pages

--- app/lib/app_shell/presentation/shell_page.dart
+++ app/lib/app_shell/presentation/shell_page.dart
@@ -1,14 +1,46 @@
 import 'package:flutter/material.dart';
+import '../../plants/presentation/plants_page.dart';
+import '../../calendar/presentation/calendar_page.dart';
+import '../../settings/presentation/settings_page.dart';
 
-class AppShellPage extends StatelessWidget {
+class AppShellPage extends StatefulWidget {
   const AppShellPage({super.key});
+
+  @override
+  State<AppShellPage> createState() => _AppShellPageState();
+}
+
+class _AppShellPageState extends State<AppShellPage> {
+  int _index = 0;
+
+  final _pages = const [
+    PlantsPage(),
+    CalendarPage(),
+    SettingsPage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Leafy 🌱')),
-      body: const Center(
-        child: Text('Hello Leafy', style: TextStyle(fontSize: 24)),
+      body: _pages[_index],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _index,
+        onTap: (i) => setState(() => _index = i),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.local_florist),
+            label: 'Plants',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_month),
+            label: 'Calendar',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
       ),
     );
   }
